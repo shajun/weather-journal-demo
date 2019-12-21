6589f373e920543845ceef7ac01e4825ad9a950f
shajun <junco.sha@gmail.com>
2019-12-22 01:04:48 +0800

    Add project template files

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-const http = require('http');
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
 
-//  create a server object:
-http
-  .createServer(function(req, res) {
-    res.write('Hello, World!');
-    res.end();
-  })
-  .listen(8000);
+// Require Express to run server and routes
+
+// Start up an instance of app
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+// Setup Server
